32d5df13cdd82d67494b268fef0116cedb3fa0f0
Guillaume Bayard <guillaume.bayard@grenoble-inp.org>
2019-01-07 07:47:23 +0100

    maj

--- proxy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-
-sudo export http_proxy="www-cache.ujf-grenoble.fr:3128"
-sudo export https_proxy="www-cache.ujf-grenoble.fr:3128"
--- setProxy.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-sudo cp apt.conf /etc/apt/
-sudo cp apt.conf /etc/apt/apt.conf.d/proxy
